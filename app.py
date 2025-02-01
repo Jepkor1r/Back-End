8c60f60cb053289f3aca2f6144c683c25bdd4f8c
Jepkor1r <jlagat007@gmail.com>
2025-02-01 20:23:29 +0300

    fix root error page in render

--- app.py
+++ app.py
@@ -8,6 +8,16 @@ app = Flask(__name__)
 #Enable CORS for all routes
 CORS(app)
 
+@app.route('/')
+def home():
+    # Return JSON response directly at the root route
+    data = {
+        "message": "Welcome to the Flask API!",
+        "current_datetime": datetime.utcnow().isoformat() + "Z",
+        "github_url": "https://github.com/Jepkor1r/Back-End"
+    }
+    return jsonify(data)
+
 @app.route('/api/info', methods=['GET'])
 def get_info():
 
