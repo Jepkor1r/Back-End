ec94804f1908d7bdfed6657f1cde4adfd2daa82b
Jepkor1r <jlagat007@gmail.com>
2025-02-01 18:49:38 +0300

    trying to fix cosr error

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
-from flask import Flask
+
+from flask import Flask, jsonify
 from flask_cors import CORS
-from flask import jsonify
 from datetime import datetime
 
 app = Flask(__name__)
 
-#Enable CORS for all routes and all origins
-CORS(app, resources={r"/*": {"origins": "*"}})
+#Enable CORS for all routes
+CORS(app)
 
 @app.route('/api/info', methods=['GET'])
 def get_info():
 
+    # Handle the actual GET request 
     data = {
     "email": "jlagat007@gmail.com",
     "current_datetime": datetime.utcnow().isoformat() + "Z",
     "github_url": "https://github.com/Jepkor1r/Back-End"
     }
+
     return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
